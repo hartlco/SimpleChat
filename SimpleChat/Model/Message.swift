b18786f24e7b1c2d245f8fa9dc0e047a7de0e335
Martin Hartl <martin@hartl.co>
2016-11-15 14:54:06 +0100

    Add message model

---
+++ SimpleChat/Model/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  SimpleChat
+//
+//  Created by mhaddl on 15/11/2016.
+//  Copyright © 2016 Martin Hartl. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let username: String
+    let message: String
+    let date: Date
+}
